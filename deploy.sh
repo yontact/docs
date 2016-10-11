1af69c69ebd56ef297fb7099863824c53f3e87fe
Chen Wei <acee06.weichen@gmail.com>
2016-10-11 12:52:07 +0800

    deploy...

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 #
-export PATH=/Applications/calibre.app/Contents/MacOS:$PATH
-gitbook pdf
+# export PATH=/Applications/calibre.app/Contents/MacOS:$PATH
+# gitbook pdf
 
 gitbook build
 cd _book
 rm deploy.sh gendocx.* .gitignore
 cp ../book.pdf download/mate/云镜说明书-完整版.pdf
-git init
-git remote add origin git@git.coding.net:chenweiyj/yontact-gitbook.git
+cd ..
+mkdir -p _publish
+mv _book/* _publish/
+
+git remote -v
+status=$?
+if [[ status -ne 0 ]]; then
+  git init
+  git remote add origin git@git.coding.net:chenweiyj/yontact-gitbook.git
+fi
+
 git add -A
 git commit -m 'deploy...'
-git push -f -u origin master
+git push -u origin master
 
 
